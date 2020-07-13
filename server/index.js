1eedd401a4ea331324806956b6cf22445ead36b9
Brian Sai <brianwsai@gmail.com>
2020-07-13 15:55:48 -0700

    Retrieved data from postgres through booking route

--- server/index.js
+++ server/index.js
@@ -2,11 +2,14 @@ const express = require('express');
 const path = require('path');
 const dotenv = require('dotenv');
 const app = express();
+const { booking } = require('./routes/booking.js');
 const db = require('./db/pgConfig.js');
 
 dotenv.config();
 
 app.use(express.static(path.join(__dirname, '../client/dist')));
+
+app.use(booking);
 
 db.connect()
   .then(() => {
