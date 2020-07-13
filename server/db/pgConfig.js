f4a28b049c9d0ea2085594a85caf599e75ad235a
Brian Sai <brianwsai@gmail.com>
2020-07-13 14:52:39 -0700

    Connected and input booking data to Postgres

---
+++ server/db/pgConfig.js
@@ -0,0 +1,13 @@
+const Pool = require('pg').Pool;
+const dotenv = require('dotenv');
+const envConfig = dotenv.config({ silent: true });
+
+const pool = new Pool({
+  database: process.env.DB_DATABASE,
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT
+});
+
+module.exports = pool;
--- server/index.js
+++ server/index.js
@@ -2,10 +2,22 @@ const express = require('express');
 const path = require('path');
 const dotenv = require('dotenv');
 const app = express();
+const db = require('./db/pgConfig.js');
+
 dotenv.config();
 
 app.use(express.static(path.join(__dirname, '../client/dist')));
 
-app.listen(process.env.PORT, () => {
-  console.log(`Listening on port: ${process.env.PORT}`);
-});
+const startConnection = async () => {
+  try {
+    await db.connect();
+
+    app.listen(process.env.PORT, () => {
+      console.log(`Listening on port: ${process.env.PORT}`);
+    });
+  } catch (err) {
+    throw new Error();
+  }
+};
+
+startConnection();
