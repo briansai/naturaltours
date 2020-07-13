d2af78e1132986bba323c194237180ae3bb439d1
Brian Sai <brianwsai@gmail.com>
2020-07-13 15:23:15 -0700

    Refactored connection

--- server/index.js
+++ server/index.js
@@ -8,16 +8,12 @@ dotenv.config();
 
 app.use(express.static(path.join(__dirname, '../client/dist')));
 
-const startConnection = async () => {
-  try {
-    await db.connect();
-
+db.connect()
+  .then(() => {
     app.listen(process.env.PORT, () => {
       console.log(`Listening on port: ${process.env.PORT}`);
     });
-  } catch (err) {
-    throw new Error();
-  }
-};
-
-startConnection();
+  })
+  .catch(err => {
+    console.log('Connection error');
+  });
