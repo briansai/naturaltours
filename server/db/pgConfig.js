a2d85bd674921280b0ad0a2493546a3dafd75045
Brian Sai <brianwsai@gmail.com>
2020-07-15 19:04:55 -0700

    Created dockerfile and connected to Postgres in ec2 instance

--- server/db/pgConfig.js
+++ server/db/pgConfig.js
@@ -1,13 +1,13 @@
 const Pool = require('pg').Pool;
 const dotenv = require('dotenv');
-dotenv.config({ silent: true });
+dotenv.config({ silent: 'true' });
 
 const pool = new Pool({
-  database: process.env.DB_DATABASE,
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT
+  database: process.env.POSTGRES_DATABASE,
+  user: process.env.POSTGRES_USER,
+  host: process.env.POSTGRES_HOST,
+  password: process.env.POSTGRES_PASSWORD,
+  port: process.env.POSTGRES_PORT
 });
 
 module.exports = pool;
---
+++ server/db/schema.sql
@@ -0,0 +1,42 @@
+\c naturaltours;
+
+DROP TABLE IF EXISTS adventure CASCADE;
+DROP TABLE IF EXISTS features CASCADE;
+DROP TABLE IF EXISTS main CASCADE;
+
+CREATE TABLE adventure (
+  id SERIAL PRIMARY KEY,
+  adventure VARCHAR(50) NOT NULL,
+  city VARCHAR(50) NOT NULL,
+  country CHAR(3) NOT NULL,
+  start_date VARCHAR(15) NOT NULL,
+  days INT NOT NULL,
+  difficulty VARCHAR(10) NOT NULL,
+  stops INT NOT NULL,
+  detail VARCHAR(100) NOT NULL,
+  about VARCHAR(300) NOT NULL,
+  max_people INT NOT NULL,
+  price INT NOT NULL,
+  rating INT NOT NULL,
+  image VARCHAR(200) NOT NULL
+);
+
+CREATE TABLE features (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(10) NOT NULL,
+  source VARCHAR(100) NOT NULL,
+  title VARCHAR(50) NOT NULL,
+  text VARCHAR(150) NOT NULL
+);
+
+CREATE TABLE main (
+  id SERIAL PRIMARY KEY,
+  features_id INT NOT NULL,
+  adventure_id INT NOT NULL
+);
+
+\COPY adventure (id,adventure,city,country,start_date, days, difficulty,stops,detail,about,max_people,price,rating, image) FROM './data.csv' DELIMITER ',' CSV HEADER;
+\COPY features (id,name,source,title,text) FROM './feature.csv' DELIMITER ',' CSV HEADER;
+
+ALTER TABLE main ADD CONSTRAINT features_fk FOREIGN KEY (features_id) REFERENCES features (id);
+ALTER TABLE main ADD CONSTRAINT adventure_fk FOREIGN KEY (adventure_id) REFERENCES adventure (id);
